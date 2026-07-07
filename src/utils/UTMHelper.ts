3bc3fe97fcfc7b2e4fb1b24c41d0cb7dd969f8cf
dodhee <dodhee@gmail.com>
2026-07-07 19:44:44 +0700

    refactor(godev): clean up unused template files and blog content

---
+++ src/utils/UTMHelper.ts
@@ -0,0 +1,12 @@
+export function getUTMMessage(url: string, defaultMessage: string): string {
+  try {
+    const searchParams = new URL(url).searchParams;
+    const utmCampaign = searchParams.get('utm_campaign');
+
+    if (!utmCampaign) return defaultMessage;
+
+    return `Halo godev, saya tertarik dengan solusi ${utmCampaign.toUpperCase()} tapi pusing setup-nya.`;
+  } catch {
+    return defaultMessage;
+  }
+}
